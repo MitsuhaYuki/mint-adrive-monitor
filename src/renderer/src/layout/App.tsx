617cb38438be3505b1c83004fdf5e412dd393327
MitsuhaYuki <46064690+MitsuhaYuki@users.noreply.github.com>
2025-11-18 18:19:04 +0800

    build: add portable build support and adjust window settings

--- src/renderer/src/layout/App.tsx
+++ src/renderer/src/layout/App.tsx
@@ -1,15 +1,28 @@
 import { FC } from 'react'
 import { StyleProvider } from '@ant-design/cssinjs'
+import { App, ConfigProvider, theme } from 'antd'
 
 interface IProps {}
 
 const Content: FC<IProps> = (props) => {
   // eslint-disable-next-line
   const {} = props
-  console.log('I: props:', props)
+
   return (
     <StyleProvider layer>
-      <div className='w-full h-full'>App</div>
+      <ConfigProvider
+        theme={{ algorithm: theme.darkAlgorithm }}
+        componentSize="small"
+        button={{ autoInsertSpace: false }}
+      >
+        <App
+          message={{ maxCount: 3 }}
+          notification={{ bottom: 6, duration: 0, placement: 'bottomRight' }}
+          className='w-full h-full bg-neutral-950'
+        >
+          <div className='w-full h-full bg-neutral-800'>App</div>
+        </App>
+      </ConfigProvider>
     </StyleProvider>
   )
 }
